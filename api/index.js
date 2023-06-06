61b1c531b519d655e4c87c09524779d5ff4d2021
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-06-06 15:12:52 +0200

    test to fix nodemon port issue

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.APP_PORT || 8080
+const port = process.env.APP_PORT || 5002;
 const APIRouter = express.Router();
 
 //resolving cors issue from fetching from diffrent origins
