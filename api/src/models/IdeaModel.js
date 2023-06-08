4e7cb5edc20d08f2df1cdc7f7280a03d51e4a6a6
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-06-08 11:09:26 +0200

    idea routes all and by ID OK exept comments

--- api/src/models/IdeaModel.js
+++ api/src/models/IdeaModel.js
@@ -2,58 +2,88 @@ const BaseModel = require('./BaseModel');
 
 class IdeaModel extends BaseModel {
 
-    queryFields;
+  queryFields;
 
-    constructor({fields}){
-        super('ideas')
-        
-        this.init(fields && fields.split(','));
+  constructor({ fields }) {
+    super('ideas')
 
-        // this.fields = [
-        //         `categories.label AS category`,
-        //         `ideas.id`,
-        //         `ideas.title`,
-        //         `locations.city`,
-        //         `locations.region`,
-        //         `locations.country`,
-        //         `status.label AS status`,
-        //         `status.delay`
-        //     ]
-        // this.join = [
-        //     `LEFT JOIN ideas_has_categories ON ideas.id = ideas_has_categories.id_idea
-        //     LEFT JOIN categories ON ideas_has_categories.id_category = categories.id
-        //     LEFT JOIN ideas_has_locations ON ideas.id = ideas_has_locations .id_idea
-        //     LEFT JOIN locations ON ideas_has_locations.id_location = locations.id
-        //     LEFT JOIN status ON ideas.id_status = status.id`
-        // ]
-    }
+    //securisation si fields vide et split de la chaine issue de la query
+    this.init(fields && fields.split(','));
+
+  }
 
 
-
-    init(fields) {
-      if(!fields){
-        this.fields.push('*')
-      } else {
+  //si la query est vide, on affiche toutes les idees, sinon uniquement les donnes pour la card
+  init(fields) {
+    if (!fields) {
+      this.fields.push('*')
+    } else {
       this.queryFields = fields
+      // id,title,categories,locations,status
 
-        // id,title,categories,locations,status
-     
-        if(this.queryFields.includes('id')) {
-            this.fields.push(`ideas.id`)
-        }
-        if(this.queryFields.includes('title')) {
-            this.fields.push(`ideas.title`)
-        }
-        if(this.queryFields.includes('status')) {
-            this.fields.push(`status.label AS status_label`)
-            this.fields.push(`status.delay AS status_delay`)
+      if (this.queryFields.includes('id')) {
+        this.fields.push(`ideas.id`)
+      }
+      if (this.queryFields.includes('title')) {
+        this.fields.push(`ideas.title`)
+      }
+      if (this.queryFields.includes('deadline')) {
+        this.fields.push(`ideas.deadline`)
+      }
+      if (this.queryFields.includes('detail')) {
+        this.fields.push(`ideas.detail`)
+      }
+      if (this.queryFields.includes('risk')) {
+        this.fields.push(`ideas.risk`)
+      }
+      if (this.queryFields.includes('benefit')) {
+        this.fields.push(`ideas.benefit`)
+      }
+      if (this.queryFields.includes('impact')) {
+        this.fields.push(`ideas.impact`)
+      }
 
-            this.join.push(`LEFT JOIN status ON ideas.id_status = status.id`)
-        }
+      if (this.queryFields.includes('users')) {
+        this.fields.push(`users.id AS user_id`)
+        this.fields.push(`users.firstname`)
+        this.fields.push(`users.lastname`)
+        this.fields.push(`users.picture`)
+        this.fields.push(`users_has_ideas.is_owner`)
+        this.fields.push(`roles.label AS role`)
 
+      }
+
+      // if (this.queryFields.includes('comments')) {
+      //   this.fields.push(`comments.id AS comment_id`)
+      //   this.fields.push(`comments.content`)
+      // }
+
+      if (this.queryFields.includes('categories')) {
+        this.fields.push(`categories.label AS category`)
+      }
+      if (this.queryFields.includes('locations')) {
+        this.fields.push(`locations.city`)
+      }
+
+      if (this.queryFields.includes('status')) {
+        this.fields.push(`status.label AS status`)
+        this.fields.push(`status.delay`)
+      }
+
+      this.join.push(`LEFT JOIN ideas_has_categories ON ideas.id = ideas_has_categories.id_idea
+        LEFT JOIN categories ON ideas_has_categories.id_category = categories.id
+        LEFT JOIN ideas_has_locations ON ideas.id = ideas_has_locations .id_idea
+        LEFT JOIN locations ON ideas_has_locations.id_location = locations.id
+        LEFT JOIN status ON ideas.id_status = status.id 
+        LEFT JOIN users_has_ideas ON ideas.id = users_has_ideas.id_idea
+        LEFT JOIN users ON users_has_ideas.id_user = users.id
+        LEFT JOIN roles ON users.id_role = roles.id
+        `)
     }
   }
-    
+
 }
 
 module.exports = IdeaModel
+
+
--- api/src/routes/IdeaRoute.js
+++ api/src/routes/IdeaRoute.js
@@ -4,7 +4,7 @@ const { IdeaController } = require('../controllers')
 const ideaRouter = Router();
 
 ideaRouter.get("", (req, res) => new IdeaController(req,res).getAll())
-// ideaRouter.get("/header", (req, res) => new IdeaController(req,res).getIdeaHeader())
+ideaRouter.get("/:id", (req, res) => new IdeaController(req,res).getById())
 // ideaRouter.get("/header/:id", (req, res) => new IdeaController(req,res).getIdeaHeaderById())
 
 module.exports = ideaRouter;
