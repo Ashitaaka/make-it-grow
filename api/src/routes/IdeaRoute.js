8109574fbc06555f9b453be817edfdaa982e268c
Olivier Gomez <ashitaaaka@gmail.com>
2023-06-08 09:52:36 +0200

    moving routes order

--- api/src/routes/IdeaRoute.js
+++ api/src/routes/IdeaRoute.js
@@ -4,7 +4,7 @@ const { IdeaController } = require('../controllers')
 const ideaRouter = Router();
 
 ideaRouter.get("", (req, res) => new IdeaController(req,res).getAll())
-ideaRouter.get("/:id", (req, res) => new IdeaController(req,res).getById())
 ideaRouter.get("/test", (req, res) => new IdeaController(req,res).getCardHeader())
+ideaRouter.get("/test/:id", (req, res) => new IdeaController(req,res).getById())
 
 module.exports = ideaRouter;
