a33287160de1e13a42fa9efcc43159661d877346
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-06-09 11:40:08 +0200

    modify path to picture + axios on user route validated

--- client/src/components/ProfileDetails.jsx
+++ client/src/components/ProfileDetails.jsx
@@ -7,19 +7,28 @@ import { useParams } from 'react-router-dom'
 const ProfileDetails = () => {
 
     const {userid} = useParams()
-    const [user, setUser] = useState([])
-
+    const [user, setUser] = useState({})
+    const [isDataLoaded, setIsDataLoaded] = useState(false)
 
     useEffect(()=>{
     axios.get(`http://localhost:5080/api/users/${userid}/?fields=id,firstname,lastname,picture,occupation,locations,email`)
-        .then((res)=> setUser(res.data))
+        .then((res)=>(res.data))
+        .then((data) => {
+          setUser(...data)
+          setIsDataLoaded(true)
+        })
         
 },[])
 
-console.log(user);
 
-  return (
-    <div>Profile</div>
+return !isDataLoaded ?  null : 
+
+ (
+      <>
+        <p>{user.firstname}</p>
+        <img src='/images/profile-pictures/martin.jpg' alt="" />
+      </>
+
   )
 }
 
