d3ced96871a22dc2db4fa7d74b35f2cabea0436c
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-06-09 15:01:40 +0200

    minor fixes

--- client/src/components/ideas/ideaExtended/ideaExtendedHeader/IdeaExtendedHeader.jsx
+++ client/src/components/ideas/ideaExtended/ideaExtendedHeader/IdeaExtendedHeader.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './ideaextendedheader.css';
 
 const IdeaExtendedHeader = () => {
+  axios;
   return <div>IdeaExtendedHeader</div>;
 };
 
