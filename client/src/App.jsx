27af23228ce3d9b53c7c355563df2b52004e5892
Olivier Gomez <ashitaaaka@gmail.com>
2023-06-09 15:21:04 +0200

    sidebar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import {Routes, Route} from 'react-router-dom'
+//import components
 import Profile from './pages/Profile'
 import Home from './pages/Home'
+import Sidebar from './components/sidebar/Sidebar'
+//import css
+import './app.css'
 
 function App() {
 
   return (
       <div className='app'>
+        <Sidebar />
         <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/profile/:id' element={<Profile/>}/>
