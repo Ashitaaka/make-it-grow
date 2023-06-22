ea7256127e9118d30d844ebe5b24170392fe3b3e
Guillaume Leclout <124732293+MrG-L@users.noreply.github.com>
2023-06-22 16:09:23 +0200

    merge dev

--- client/src/components/ideas/ideaExtended/ideaExtendedDetails/IdeaExtendedDetails.jsx
+++ client/src/components/ideas/ideaExtended/ideaExtendedDetails/IdeaExtendedDetails.jsx
@@ -1,64 +1,22 @@
-import { useState } from 'react';
 import Monochev from '../../../../assets/icons/mono_chevrons_icone.svg';
-import MonochevBlanc from '../../../../assets/icons/mono_chevrons_icone_blanc.svg';
 import './ideaextendeddetails.css';
 
 const IdeaExtendedDetails = ({ idea, users }) => {
-  console.log(users);
-
   const owner = users.find((user) => user.is_owner);
-
-  const [expandedSections, setExpandedSections] = useState({
-    details: true,
-    impact: false,
-    benefits: false,
-    risks: false,
-    comments: false,
-  });
-
-  const toggleContent = (section) => {
-    setExpandedSections((prevState) => ({
-      ...prevState,
-      [section]: !prevState[section],
-    }));
-  };
   return (
     <div className="idea-details-container">
-      {/* ------------------------------------ page detail */}
-      <div className={`details ${expandedSections.details ? 'active' : ''}`}>
+      {/* //------------------------------------ page detail */}
+
+      <div className="details">
         <div className="header">
           <div className="title">
             <div
-              style={{
-                backgroundColor: expandedSections.details
-                  ? `var(${idea.color})`
-                  : 'transparent',
-                border: expandedSections.details
-                  ? 'none'
-                  : `2px solid var(${idea.color})`,
-              }}
+              style={{ backgroundColor: `var(${idea.color})` }}
               className="categorie"></div>
             <h2>Détails de l'idée</h2>
           </div>
-          <div className="cross" onClick={() => toggleContent('details')}>
-            {expandedSections.details ? (
-              <img
-                src={MonochevBlanc}
-                alt="chevron"
-                style={{
-                  backgroundColor: `var(${idea.color})`,
-                  transform: 'rotate(270deg)',
-                }}
-              />
-            ) : (
-              <img
-                src={Monochev}
-                alt="chevron"
-                style={{
-                  backgroundColor: 'var(--ultra-light-color)',
-                }}
-              />
-            )}
+          <div className="cross">
+            <img src={Monochev} alt="chevron" />
           </div>
         </div>
 
@@ -66,6 +24,7 @@ const IdeaExtendedDetails = ({ idea, users }) => {
           <div className="title">
             <h3>{idea.title}</h3>
           </div>
+
           <div className="p-content">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
@@ -82,247 +41,158 @@ const IdeaExtendedDetails = ({ idea, users }) => {
           </div>
         </div>
       </div>
-      {/* ------------------------------------ impact detail */}
-      <div className={`impact ${expandedSections.impact ? 'active' : ''}`}>
+      <hr className="separator" />
+
+      {/* //------------------------------------ impact detail */}
+
+      <div className="impact">
         <div className="header">
           <div className="title">
             <div
-              style={{
-                backgroundColor: expandedSections.impact
-                  ? `var(${idea.color})`
-                  : 'transparent',
-                border: expandedSections.impact
-                  ? 'none'
-                  : `2px solid var(${idea.color})`,
-              }}
+              style={{ backgroundColor: `var(${idea.color})` }}
               className="categorie"></div>
             <h2>Impact sur l'organisation</h2>
           </div>
-          <div className="cross" onClick={() => toggleContent('impact')}>
-            {expandedSections.impact ? (
-              <img
-                src={MonochevBlanc}
-                alt="chevron"
-                style={{
-                  backgroundColor: `var(${idea.color})`,
-                  transform: 'rotate(270deg)',
-                }}
-              />
-            ) : (
-              <img
-                src={Monochev}
-                alt="chevron"
-                style={{
-                  backgroundColor: 'var(--ultra-light-color)',
-                }}
-              />
-            )}
+          <div className="cross">
+            <img src={Monochev} alt="chevron" />
           </div>
         </div>
-        {expandedSections.impact && (
-          <div className="content-container">
-            <div className="title">
-              <h3>{idea.title}</h3>
-            </div>
-            <div className="p-content">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus rutrum libero justo, vitae pulvinar ipsum condimentum
-                blandit. Nam convallis sollicitudin libero in scelerisque. Sed
-                maximus eros in lectus sollicitudin, ac hendrerit mi cursus.
-                Nulla consequat eu nibh at faucibus. Nam volutpat non ipsum
-                vestibulum condimentum. Mauris convallis risus purus, eget
-                feugiat enim ullamcorper et. Etiam at ultrices urna, ac pharetra
-                ipsum. Maecenas eget justo purus. Pellentesque pretium ante non
-                dolor venenatis congue. Fusce maximus fringilla consequat. Orci
-                varius natoque penatibus et magnis dis parturient montes,
-                nascetur ridiculus mus.
-              </p>
-            </div>
+
+        <div className="content-container">
+          <div className="title">
+            <h3>{idea.title}</h3>
           </div>
-        )}
+
+          <div className="p-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              rutrum libero justo, vitae pulvinar ipsum condimentum blandit. Nam
+              convallis sollicitudin libero in scelerisque. Sed maximus eros in
+              lectus sollicitudin, ac hendrerit mi cursus. Nulla consequat eu
+              nibh at faucibus. Nam volutpat non ipsum vestibulum condimentum.
+              Mauris convallis risus purus, eget feugiat enim ullamcorper et.
+              Etiam at ultrices urna, ac pharetra ipsum. Maecenas eget justo
+              purus. Pellentesque pretium ante non dolor venenatis congue. Fusce
+              maximus fringilla consequat. Orci varius natoque penatibus et
+              magnis dis parturient montes, nascetur ridiculus mus.
+            </p>
+          </div>
+        </div>
       </div>
-      {/* ------------------------------------ bénéfices detail */}
-      <div className={`benefits ${expandedSections.benefits ? 'active' : ''}`}>
+      <hr className="separator" />
+
+      {/* //------------------------------------ bénéfices detail */}
+      <div className="benefits">
         <div className="header">
           <div className="title">
             <div
-              style={{
-                backgroundColor: expandedSections.benefits
-                  ? `var(${idea.color})`
-                  : 'transparent',
-                border: expandedSections.benefits
-                  ? 'none'
-                  : `2px solid var(${idea.color})`,
-              }}
+              style={{ backgroundColor: `var(${idea.color})` }}
               className="categorie"></div>
             <h2>Bénéfices</h2>
           </div>
-          <div className="cross" onClick={() => toggleContent('benefits')}>
-            {expandedSections.benefits ? (
-              <img
-                src={MonochevBlanc}
-                alt="chevron"
-                style={{
-                  backgroundColor: `var(${idea.color})`,
-                  transform: 'rotate(270deg)',
-                }}
-              />
-            ) : (
-              <img
-                src={Monochev}
-                alt="chevron"
-                style={{
-                  backgroundColor: 'var(--ultra-light-color)',
-                }}
-              />
-            )}
+          <div className="cross">
+            <img src={Monochev} alt="chevron" />
           </div>
         </div>
-        {expandedSections.benefits && (
-          <div className="content-container">
-            <div className="title">
-              <h3>
-                Par {owner.firstname} {owner.lastname}
-              </h3>
-            </div>
-            <div className="p-content">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus rutrum libero justo, vitae pulvinar ipsum condimentum
-                blandit. Nam convallis sollicitudin libero in scelerisque. Sed
-                maximus eros in lectus sollicitudin, ac hendrerit mi cursus.
-                Nulla consequat eu nibh at faucibus. Nam volutpat non ipsum
-                vestibulum condimentum. Mauris convallis risus purus, eget
-                feugiat enim ullamcorper et. Etiam at ultrices urna, ac pharetra
-                ipsum. Maecenas eget justo purus. Pellentesque pretium ante non
-                dolor venenatis congue. Fusce maximus fringilla consequat. Orci
-                varius natoque penatibus et magnis dis parturient montes,
-                nascetur ridiculus mus.
-              </p>
-            </div>
+
+        <div className="content-container">
+          <div className="title">
+            <h3>
+              Par {owner.firstname} {owner.lastname}
+            </h3>
           </div>
-        )}
+
+          <div className="p-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              rutrum libero justo, vitae pulvinar ipsum condimentum blandit. Nam
+              convallis sollicitudin libero in scelerisque. Sed maximus eros in
+              lectus sollicitudin, ac hendrerit mi cursus. Nulla consequat eu
+              nibh at faucibus. Nam volutpat non ipsum vestibulum condimentum.
+              Mauris convallis risus purus, eget feugiat enim ullamcorper et.
+              Etiam at ultrices urna, ac pharetra ipsum. Maecenas eget justo
+              purus. Pellentesque pretium ante non dolor venenatis congue. Fusce
+              maximus fringilla consequat. Orci varius natoque penatibus et
+              magnis dis parturient montes, nascetur ridiculus mus.
+            </p>
+          </div>
+        </div>
       </div>
-      {/* ------------------------------------ Risque detail */}
-      <div className={`risks ${expandedSections.risks ? 'active' : ''}`}>
+      <hr className="separator" />
+
+      {/* //------------------------------------ Risque detail */}
+
+      <div className="risks">
         <div className="header">
           <div className="title">
             <div
-              style={{
-                backgroundColor: expandedSections.risks
-                  ? `var(${idea.color})`
-                  : 'transparent',
-                border: expandedSections.risks
-                  ? 'none'
-                  : `2px solid var(${idea.color})`,
-              }}
+              style={{ backgroundColor: `var(${idea.color})` }}
               className="categorie"></div>
-            <h2>Risques</h2>
+            <h2>Impact sur l'organisation</h2>
           </div>
-          <div className="cross" onClick={() => toggleContent('risks')}>
-            {expandedSections.risks ? (
-              <img
-                src={MonochevBlanc}
-                alt="chevron"
-                style={{
-                  backgroundColor: `var(${idea.color})`,
-                  transform: 'rotate(270deg)',
-                }}
-              />
-            ) : (
-              <img
-                src={Monochev}
-                alt="chevron"
-                style={{
-                  backgroundColor: 'var(--ultra-light-color)',
-                }}
-              />
-            )}
+          <div className="cross">
+            <img src={Monochev} alt="chevron" />
           </div>
         </div>
-        {expandedSections.risks && (
-          <div className="content-container">
-            <div className="title">
-              <h3>{idea.title}</h3>
-            </div>
-            <div className="p-content">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus rutrum libero justo, vitae pulvinar ipsum condimentum
-                blandit. Nam convallis sollicitudin libero in scelerisque. Sed
-                maximus eros in lectus sollicitudin, ac hendrerit mi cursus.
-                Nulla consequat eu nibh at faucibus. Nam volutpat non ipsum
-                vestibulum condimentum. Mauris convallis risus purus, eget
-                feugiat enim ullamcorper et. Etiam at ultrices urna, ac pharetra
-                ipsum. Maecenas eget justo purus. Pellentesque pretium ante non
-                dolor venenatis congue. Fusce maximus fringilla consequat. Orci
-                varius natoque penatibus et magnis dis parturient montes,
-                nascetur ridiculus mus.
-              </p>
-            </div>
+
+        <div className="content-container">
+          <div className="title">
+            <h3>{idea.title}</h3>
           </div>
-        )}
+
+          <div className="p-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              rutrum libero justo, vitae pulvinar ipsum condimentum blandit. Nam
+              convallis sollicitudin libero in scelerisque. Sed maximus eros in
+              lectus sollicitudin, ac hendrerit mi cursus. Nulla consequat eu
+              nibh at faucibus. Nam volutpat non ipsum vestibulum condimentum.
+              Mauris convallis risus purus, eget feugiat enim ullamcorper et.
+              Etiam at ultrices urna, ac pharetra ipsum. Maecenas eget justo
+              purus. Pellentesque pretium ante non dolor venenatis congue. Fusce
+              maximus fringilla consequat. Orci varius natoque penatibus et
+              magnis dis parturient montes, nascetur ridiculus mus.
+            </p>
+          </div>
+        </div>
       </div>
-      {/* ------------------------------------ Comment detail */}
-      <div className={`comments ${expandedSections.comments ? 'active' : ''}`}>
+      <hr className="separator" />
+
+      {/* //------------------------------------ Commentaire detail */}
+
+      <div className="comments">
         <div className="header">
           <div className="title">
             <div
-              style={{
-                backgroundColor: expandedSections.comments
-                  ? `var(${idea.color})`
-                  : 'transparent',
-                border: expandedSections.comments
-                  ? 'none'
-                  : `2px solid var(${idea.color})`,
-              }}
+              style={{ backgroundColor: `var(${idea.color})` }}
               className="categorie"></div>
             <h2>Commentaires</h2>
           </div>
-          <div className="cross" onClick={() => toggleContent('comments')}>
-            {expandedSections.comments ? (
-              <img
-                src={MonochevBlanc}
-                alt="chevron"
-                style={{
-                  backgroundColor: `var(${idea.color})`,
-                  transform: 'rotate(270deg)',
-                }}
-              />
-            ) : (
-              <img
-                src={Monochev}
-                alt="chevron"
-                style={{
-                  backgroundColor: 'var(--ultra-light-color)',
-                }}
-              />
-            )}
+          <div className="cross">
+            <img src={Monochev} alt="chevron" />
           </div>
         </div>
-        {expandedSections.comments && (
-          <div className="content-container">
-            <div className="title">
-              <h3>{idea.title}</h3>
-            </div>
-            <div className="p-content">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus rutrum libero justo, vitae pulvinar ipsum condimentum
-                blandit. Nam convallis sollicitudin libero in scelerisque. Sed
-                maximus eros in lectus sollicitudin, ac hendrerit mi cursus.
-                Nulla consequat eu nibh at faucibus. Nam volutpat non ipsum
-                vestibulum condimentum. Mauris convallis risus purus, eget
-                feugiat enim ullamcorper et. Etiam at ultrices urna, ac pharetra
-                ipsum. Maecenas eget justo purus. Pellentesque pretium ante non
-                dolor venenatis congue. Fusce maximus fringilla consequat. Orci
-                varius natoque penatibus et magnis dis parturient montes,
-                nascetur ridiculus mus.
-              </p>
-            </div>
+
+        <div className="content-container">
+          <div className="title">
+            <h3>{idea.title}</h3>
           </div>
-        )}
+
+          <div className="p-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              rutrum libero justo, vitae pulvinar ipsum condimentum blandit. Nam
+              convallis sollicitudin libero in scelerisque. Sed maximus eros in
+              lectus sollicitudin, ac hendrerit mi cursus. Nulla consequat eu
+              nibh at faucibus. Nam volutpat non ipsum vestibulum condimentum.
+              Mauris convallis risus purus, eget feugiat enim ullamcorper et.
+              Etiam at ultrices urna, ac pharetra ipsum. Maecenas eget justo
+              purus. Pellentesque pretium ante non dolor venenatis congue. Fusce
+              maximus fringilla consequat. Orci varius natoque penatibus et
+              magnis dis parturient montes, nascetur ridiculus mus.
+            </p>
+          </div>
+        </div>
       </div>
       <div className="cta-button-container">
         <button className="cta-button">Voter</button>
@@ -331,4 +201,5 @@ const IdeaExtendedDetails = ({ idea, users }) => {
     </div>
   );
 };
+
 export default IdeaExtendedDetails;
--- client/src/components/ideas/ideasCard/IdeasCardsBackground.jsx
+++ client/src/components/ideas/ideasCard/IdeasCardsBackground.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import IdeasCard from './IdeasCard'
+import IdeasCard from './IdeasCard';
 
-import './IdeasCard.css'
-
+import './IdeasCard.css';
 
 const IdeasCardsBackground = () => {
+  const [ideas, setIdeas] = useState([]);
 
-    const [ideas, setIdeas] = useState([])
-
-    useEffect(()=>{
-    axios.get(`http://localhost:5080/api/ideas/?fields=id,title,locations,status,categories,users`)
-        .then((res)  => res.data)
-        .then((data)=> setIdeas(data))
-    },[])
-
-   
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:5080/api/ideas/?fields=id,title,locations,status,categories,users`
+      )
+      .then((res) => res.data)
+      .then((data) => setIdeas(data));
+  }, []);
 
   return (
-    <div className='ideas_cards_background'>
-        {ideas.map((idea, index) => (
-            <IdeasCard idea={idea} key={index} />
-        ))}
-        
+    <div className="ideas_cards_background">
+      {ideas.map((idea, index) => (
+        <IdeasCard idea={idea} key={index} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default IdeasCardsBackground
+export default IdeasCardsBackground;
