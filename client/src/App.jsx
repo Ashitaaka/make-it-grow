c9b221a6834e5959c325a78c546144d0075908f4
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-07-06 14:37:55 +0200

    create admin page in RAFCE state and CSS linked

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,56 +8,50 @@ import ProfileDetails from "./components/ProfileDetails/ProfileDetails";
 import TopBar from "./components/top bar/TopBar";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import { useUser } from "./hooks/UserContext"
+import { useUser } from "./hooks/UserContext";
 import tokenStorage from "./hooks/useToken";
-import CreateIdea from './components/ideas/createIdea/CreateIdea'
-import IdeaExtended from './components/ideas/ideaExtended/IdeaExtended';
+import CreateIdea from "./components/ideas/createIdea/CreateIdea";
+import IdeaExtended from "./components/ideas/ideaExtended/IdeaExtended";
 //import css
 import "./App.css";
 
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = "/api";
 
 function App() {
-
-
   const { removeToken, setToken, token } = tokenStorage();
-  
+
   //Which button is active?
   const [clickedButton, setClickedButton] = useState("");
 
-
-    return !token
-    ? (
-      <Routes>
-        <Route path="*" element={<Navigate to="/login"/>} />
-        <Route path="/login" element={<Login setToken={setToken} />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      
-    )
-    :(
-      <div className="app">
-        <Sidebar
+  return !token ? (
+    <Routes>
+      <Route path="*" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login setToken={setToken} />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  ) : (
+    <div className="app">
+      <Sidebar
+        clickedButton={clickedButton}
+        setClickedButton={setClickedButton}
+      />
+      <div className="app_container">
+        <TopBar
           clickedButton={clickedButton}
           setClickedButton={setClickedButton}
+          removeToken={removeToken}
+          token={token}
         />
-        <div className="app_container">
-          <TopBar
-            clickedButton={clickedButton}
-            setClickedButton={setClickedButton}
-            removeToken={removeToken}
-            token={token}
-          />
-          <Routes>
-            <Route path="*" element={<Navigate to="/dashboard"/>} />
-            <Route path="/dashboard" element={<Home />} />
-            <Route path="/profile/:userid" element={<ProfileDetails />} />
-            <Route path='/newidea' element={<CreateIdea />}/>
-            <Route path="/idea/:id" element={<IdeaExtended />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/profile/:userid" element={<ProfileDetails />} />
+          <Route path="/newidea" element={<CreateIdea />} />
+          <Route path="/idea/:id" element={<IdeaExtended />} />
+        </Routes>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/admin/Admin.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./admin.css";
+
+const Admin = () => {
+  return <div>Admin</div>;
+};
+
+export default Admin;
