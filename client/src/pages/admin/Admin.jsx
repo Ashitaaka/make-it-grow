10db04b396508c6b8f3c8ac98ad93af763bb8ada
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-07-06 16:02:10 +0200

    first try on ideas panel admin

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -16,9 +16,23 @@ const Admin = () => {
   const sortedIdeas =
     ideas && ideas.sort((a, b) => (a.id_status > b.id_status ? 1 : -1));
 
-  console.log(sortedIdeas);
-
-  return <div>{/* {ideas && ideas.map((idea) => console.log(idea))} */}</div>;
+  return (
+    <div className="admin_page">
+      <div className="admin_ideas_container">
+        <div className="admin_ideas_head">
+          <div className="admin_head_title">Titre</div>
+          <div className="admin_head_status">Status</div>
+        </div>
+        {sortedIdeas &&
+          sortedIdeas.map((idea, index) => (
+            <div key={index} className="admin_ideas_content">
+              <div className="admin_ideas_title">{idea.title}</div>
+              <div className="admin_ideas_status">{idea.status}</div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
