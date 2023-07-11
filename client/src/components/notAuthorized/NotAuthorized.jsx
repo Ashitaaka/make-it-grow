d5e44961b779b1f89b0e144e7694809375e0a014
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-07-11 15:53:23 +0200

    not authorized component

--- client/src/components/notAuthorized/NotAuthorized.jsx
+++ client/src/components/notAuthorized/NotAuthorized.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./notAuthorized.css";
+import { Link } from "react-router-dom";
 
 const NotAuthorized = () => {
-  return <div>notAuthorized</div>;
+  return (
+    <div className="not_authorized_content">
+      <div className="not_autorized_message">
+        <h3>Vous n'êtes pas autorisé</h3>
+        <Link to="/dashboard">
+          <button>OK</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotAuthorized;
