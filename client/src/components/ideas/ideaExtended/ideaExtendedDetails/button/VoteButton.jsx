13538d19cff887b5c1562bea62fd2afcc19689ac
Axelastach <axelpicaut@orange.fr>
2023-07-13 16:13:40 +0200

    update button status

--- client/src/components/ideas/ideaExtended/ideaExtendedDetails/button/VoteButton.jsx
+++ client/src/components/ideas/ideaExtended/ideaExtendedDetails/button/VoteButton.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import '../accordion/accordion.css'
 
-const ApproveOrDeclined = ({idea}) => {
-  
-
-    const navigate = useNavigate();
-    const handleUpdateIdeaStatus = () => {
-        axios.put(`ideas/${idea.idea_id}`, { id_status: 2 });
+const VoteButton = ({idea}) => {
+    const handleVoteForIdeaStatus = () => {
+        axios.put(`ideas/${idea.idea_id}`, { 
+            has_voted: 1,
+            value:1 
+        });
       };
-    const handleRejectIdeaStatus = () => {
-        axios.put(`ideas/${idea.idea_id}`, {    
-            is_closed: 1,
-            id_status: 8,
-        })
-        navigate("/dashboard")
-
+    const handleVoteAgainstIdeaStatus = () => {
+        axios.put(`ideas/${idea.idea_id}`, { 
+            has_voted: 1,
+            value:0
+        });
       };
 
   return (
@@ -37,14 +34,14 @@ const ApproveOrDeclined = ({idea}) => {
               e.target.style.color = "white";
             }}
             onClick={() => {
-              handleUpdateIdeaStatus();
-              window.location.reload(false);
+                handleVoteForIdeaStatus()
+                window.location.reload(false);
             }}
           >
-            Approuver
+            Pour
           </button>
           <button
-            className="cta-button"
+            className="cta_button_action_status"
             style={{
               backgroundColor:  `var(${idea.color})`,
               border: `2px solid var(${idea.color})`,
@@ -59,15 +56,15 @@ const ApproveOrDeclined = ({idea}) => {
               e.target.style.color = "white";
             }}
             onClick={() => {
-                handleRejectIdeaStatus ();
-              window.location.reload(false);
+                handleVoteAgainstIdeaStatus()
+                window.location.reload(false);
             }}
           >
-            Rejeter
+            Contre
           </button>
 
     </div>
   )
 }
 
-export default ApproveOrDeclined
+export default VoteButton
